17d6c42532d3f8fe1c912e63e179eeb5c8d59992
Ramon Nascimento dos santos <ramon.dev.rs@gmail.com>
2025-08-26 02:01:23 +0000

    criei o tabuleiro 10x10

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,7 +9,28 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    char linhas[10] = {'A','B','C','D','E','F','G','H','I','J'};
+        
 
+   for (int i = 0; i < 10; i++)// coloca as letras do A até o J nomeado as colunas
+   {
+    printf("%2c", linhas[i]); 
+   }
+   printf("\n");
+    
+   
+    for (int i = 1; i <= 10; i++)
+    {
+        printf("%2d ", i);// numera as linhas do 1 até 10
+   
+        for (int j = 0; j < 10; j++)//tabela de zeros 10x10
+        { 
+            printf("%2d", 0);
+        }
+        
+        printf("\n");
+       
+    }
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
     // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
