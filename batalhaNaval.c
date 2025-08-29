92d126f1aa1c0e63c83b07b1856ab6c521f295f0
Ramon Nascimento dos santos <ramon.dev.rs@gmail.com>
2025-08-29 00:44:25 +0000

    Habilidades cone, cruz, octaedro ponto de origem fixos

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,29 +1,31 @@
 #include <stdio.h>
 
+#define LINHAS 10
+#define COLUNAS 10
 int main() {
     
     char linhas[10] = {'A','B','C','D','E','F','G','H','I','J'};//vetor
-    int tabuleiro[10][10] = {0};
+    int tabuleiro[LINHAS][COLUNAS] = {0};
 
 
     printf(" TABULEIRO BATALHA NAVAL \n");
-    printf("  ");
+    
 
-   for (int i = 0; i < 10; i++)// coloca as letras de A até o J nomeado as colunas
+   for (int i = 0; i < LINHAS; i++)// coloca as letras de A até o J nomeado as colunas
    {
     printf("%2c", linhas[i]); 
    }
     printf("\n");
 
     for(int j = 3; j <= 5; j++){// Navio horizontal
-        tabuleiro[2][j] = 3; // A linha é Fixa e a coluna vai percorre index 3 4 5.
+        tabuleiro[0][j] = 3; // A linha é Fixa e a coluna vai percorre index 3 4 5.
     }
 
     for(int i = 4; i <= 6; i++){// Navio na vertical
-        tabuleiro[i][7] = 3;// A coluna é fixa e a linha percorre index 4 5 6.
+        tabuleiro[i][9] = 3;// A coluna é fixa e a linha percorre index 4 5 6.
     }
 
-    for(int i = 4, j = 1; i <= 6, j <= 3; i++, j++){// dois controle no mesmo for para fazer a diagonal esqueda
+    for(int i = 4, j = 0; i <= 6, j <= 2; i++, j++){// dois controle no mesmo for para fazer a diagonal esqueda
 
         tabuleiro[i][j] = 3;
 
@@ -33,22 +35,98 @@ int main() {
         tabuleiro[i][j] = 3;
     }
 
-    for (int i = 1; i <= 10; i++)
+    // habilidade em cone
+    for(int i = 7; i <= 9; i++)//for externo percorre as linhas de 7 a 9
+    {
+        for(int j = 0; j < COLUNAS; j++)//for interno percorre as colunas da matriz completa
+        {
+            if(i == 7 && j == 7){// primeira condição 5 sozinho
+
+                tabuleiro[i][j] = 1;
+
+            }else if (i == 8 && (j >= 6 && j <= 8))//segunda condição i fixo e J de 6 a 8
+            {
+                tabuleiro[i][j] = 1;
+
+            }else if (i == 9 && (j >= 5 && j <= 9)){// terceira condição i fixo e j de 5 a 9
+                tabuleiro[i][j] = 1;
+            }
+            
+        }
+    }
+
+    //habilidade em cruz:
+    for(int i = 7; i <= 9; i++)//for externo percorre as linhas de 7 a 9
+    {
+        for (int j = 0; j < COLUNAS; j++)//for interno percorre as colunas da matriz completa
+        {
+            if (i == 8 && (j >= 0 && j <= 4))// primeira condição i é fixo no 8 e as colunas de 0 a 4
+            {
+                tabuleiro[i][j] = 1;
+
+            }else if (i == 7 && j == 2)//segunda condição i = 7 e j = 2 
+            {
+
+                tabuleiro[i][j] = 1;
+
+            }else if (i == 9 && j == 2)//Terceira condição i = 9 e j = 2
+            {
+
+                tabuleiro[i][j] = 1;
+
+            }
+            
+        }
+        
+    }
+
+    //habilidade em octaedro
+    for(int i = 2; i <= 4; i++)//for externo percorre as linhas de 7 a 9
+    {
+        for (int j = 0; j < COLUNAS; j++)//for interno percorre as colunas da matriz completa
+        {
+            if (i == 3 && (j >= 3 && j <= 5))// primeira condição i é fixo no 8 e as colunas de 0 a 4
+            {
+                tabuleiro[i][j] = 1;
+
+            }else if (i == 2 && j == 4)//segunda condição i = 2 e j = 4 
+            {
+
+                tabuleiro[i][j] = 1;
+
+            }else if (i == 4 && j == 4)//Terceira condição i = 4 e j = 4
+            {
+
+                tabuleiro[i][j] = 1;
+
+            }
+            
+        }
+        
+    }
+
+
+
+
+
+
+
+    for (int i = 1; i <= LINHAS; i++)
     {
         printf("%2d ", i);// numera as linhas do 1 até 10
 
-        for (int j = 0; j < 10; j++)//pecorre a Matriz
+        for (int j = 0; j < COLUNAS; j++)//pecorre a Matriz
         { 
             printf("%2d", tabuleiro[i-1][j]);  
         }
         printf("\n");
     }
 
-    printf(" COORDENADAS DO NAVIO \n");
+    printf("\n COORDENADAS DO NAVIO \n");
 
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < LINHAS; i++){
 
-        for(int j = 0; j < 10; j++){
+        for(int j = 0; j < COLUNAS; j++){
 
             if(tabuleiro[i][j] == 3){
                 printf("%c%d\n", linhas[j], i+1);
